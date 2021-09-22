93778b8e4aa45c33db2260226ca5f43528a392ad
Ephraim Park <ephiepark@gmail.com>
2021-09-21 23:08:34 -0700

    firebase config set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZh7pdOTfwWqrOclagbuLHql1xgxUfdfE",
+  authDomain: "fungi-story.firebaseapp.com",
+  projectId: "fungi-story",
+  storageBucket: "fungi-story.appspot.com",
+  messagingSenderId: "104628365708",
+  appId: "1:104628365708:web:da80b4af0b2084b4240ea9",
+  measurementId: "G-4HQ08B8ZYG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
