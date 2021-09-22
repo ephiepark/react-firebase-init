bda0f834dbe3abf1964a3d795fa44e7d87d53da6
Ephraim Park <ephiepark@gmail.com>
2021-09-22 00:35:25 -0700

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import AppHeader from "./components/AppHeader";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
-import { getSignInWithEmailAndPasswordHandler, getSignUpWithEmailAndPasswordHandler } from "./auth/utils";
-import { getAuth } from "firebase/auth";
+import { getSignInWithEmailAndPasswordHandler, getSignOutHandler, getSignUpWithEmailAndPasswordHandler } from "./auth/utils";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { AuthConfig } from "./types";
 
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 // Your web app's Firebase configuration
@@ -33,53 +35,48 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth();
 
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+    console.log('login');
+  } else {
+    // User is signed out
+    // ...
+    console.log('logout');
+  }
+});
+
+const authConfig: AuthConfig = {
+  signInRoute: 'signin',
+  signInWithEmailAndPasswordHandler: getSignInWithEmailAndPasswordHandler(auth),
+  signUpRoute: 'signup',
+  signUpWithEmailAndPasswordHandler: getSignUpWithEmailAndPasswordHandler(auth),
+  signOutRoute: 'signout',
+  signOutHandler: getSignOutHandler(auth),
+};
+
 function App() {
+  console.log(auth.currentUser);
   return (
-    <div className="App">
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-      >
-        <Toolbar sx={{ flexWrap: 'wrap' }}>
-          <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-            Company name
-          </Typography>
-          <nav>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Features
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Enterprise
-            </Link>
-            <Link
-              variant="button"
-              color="text.primary"
-              href="#"
-              sx={{ my: 1, mx: 1.5 }}
-            >
-              Support
-            </Link>
-          </nav>
-          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <SignIn handleSignInWithEmailAndPassword={getSignInWithEmailAndPasswordHandler(auth)} />
-      <SignUp handleSignUpWithEmailAndPassword={getSignUpWithEmailAndPasswordHandler(auth)}/>
-    </div>
+    <Router>
+      <AppHeader authConfig={authConfig} />
+      <div className="App">
+        <Switch>
+          <Route path={'/' + authConfig.signInRoute}>
+            <SignIn handleSignInWithEmailAndPassword={authConfig.signInWithEmailAndPasswordHandler} />
+          </Route>
+          <Route path={'/' + authConfig.signOutRoute}>
+            <SignUp handleSignUpWithEmailAndPassword={authConfig.signUpWithEmailAndPasswordHandler} />
+          </Route>
+          <Route path="/">
+            <div>hi {auth.currentUser?.email}</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
