10ad540474f1daabde94acc858b051511a0f5218
Ephraim Park <ephiepark@gmail.com>
2021-09-21 23:54:11 -0700

    minor clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -40,20 +36,6 @@ const auth = getAuth();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <AppBar
         position="static"
         color="default"
